00450f3f44579de00174cfb837d659c54561344e
Giacomo Mazzamuto <mazzamuto@lens.unifi.it>
2017-03-02 09:49:54 +0100

    __main__: add function to create phase_corr op

--- stitcher/__main__.py
+++ stitcher/__main__.py
@@ -44,27 +44,15 @@ def window_filter(wz, wy, wx):
     return f
 
 
-def stitch(aname, bname, bottom, top, overlap, axis=1):
-    a = DCIMGFile(aname)
-    b = DCIMGFile(bname)
-
-    awhole = a.layer(bottom, top)
-    if axis == 2:
-        awhole = np.rot90(awhole, axes=(1, 2))
-    awhole = awhole[:, -overlap:, :]
-
-    bwhole = b.layer(bottom, top)
-    if axis == 2:
-        bwhole = np.rot90(bwhole, axes=(1, 2))
-    bwhole = bwhole[:, 0:overlap, :]
-
-    my_filter = window_filter(*awhole.shape).astype(np.float32)
-
+def phase_corr_op(ashape, bshape, filter_shape):
     with tf.name_scope('input'):
-        aph = tf.placeholder(dtype=tf.uint16, shape=awhole.shape,
+        aph = tf.placeholder(dtype=tf.uint16, shape=ashape,
                              name='a_placeholder')
-        bph = tf.placeholder(dtype=tf.uint16, shape=bwhole.shape,
+        bph = tf.placeholder(dtype=tf.uint16, shape=bshape,
                              name='b_placeholder')
+
+        my_filter_t = tf.placeholder(dtype=tf.float32, shape=filter_shape,
+                                     name='filter_placeholder')
 
         at = tf.to_float(aph)
         bt = tf.to_float(bph)
@@ -72,9 +60,6 @@ def stitch(aname, bname, bottom, top, overlap, axis=1):
     with tf.name_scope('normalize'):
         at /= tf.reduce_mean(at)
         bt /= tf.reduce_mean(bt)
-
-    my_filter_t = tf.placeholder(dtype=tf.float32, shape=my_filter.shape,
-                                 name='filter_placeholder')
 
     with tf.name_scope('window_filter'):
         at = at * my_filter_t
@@ -95,9 +80,32 @@ def stitch(aname, bname, bottom, top, overlap, axis=1):
     with tf.name_scope('phase_correlation'):
         phase_corr = tf.real(tf.ifft3d(ratio))
 
+    return phase_corr
+
+
+def stitch(aname, bname, bottom, top, overlap, axis=1):
+    a = DCIMGFile(aname)
+    b = DCIMGFile(bname)
+
+    awhole = a.layer(bottom, top)
+    if axis == 2:
+        awhole = np.rot90(awhole, axes=(1, 2))
+    awhole = awhole[:, -overlap:, :]
+
+    bwhole = b.layer(bottom, top)
+    if axis == 2:
+        bwhole = np.rot90(bwhole, axes=(1, 2))
+    bwhole = bwhole[:, 0:overlap, :]
+
+    my_filter = window_filter(*awhole.shape).astype(np.float32)
+
+    phase_corr = phase_corr_op(awhole.shape, bwhole.shape, my_filter.shape)
+
     with tf.Session() as sess:
-        phase_corr = sess.run(phase_corr, feed_dict={aph: awhole, bph: bwhole,
-                                                     my_filter_t: my_filter})
+        phase_corr = sess.run(phase_corr, feed_dict={
+            'input/a_placeholder:0': awhole,
+            'input/b_placeholder:0': bwhole,
+            'input/filter_placeholder:0': my_filter})
     tf.reset_default_graph()
 
     argmax = np.argmax(phase_corr)
