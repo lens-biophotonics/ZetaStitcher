9b44a308ff26385493e5bae74aca5c6a7e0e42c2
Giacomo Mazzamuto <mazzamuto@lens.unifi.it>
2017-03-07 16:13:28 +0100

    move TF ops to separate module

--- stitcher/core.py
+++ stitcher/core.py
@@ -9,6 +9,8 @@ from skimage.restoration import denoise_tv_bregman
 
 from dcimg import DCIMGFile
 
+from . import ops
+
 
 gpu_options = tf.GPUOptions()
 try:
@@ -47,64 +49,6 @@ def hamming_window(wz, wy, wx):
     f = f.T
 
     return f
-
-
-def phase_corr_op(ashape, bshape, filter_shape):
-    """Construct a TensorFlow op to compute phase correlation.
-
-    Parameters
-    ----------
-    ashape : tuple of ints
-        Shape of input array.
-    bshape : tuple of ints
-        Shape of input array.
-    filter_shape : tuple
-        Shape of filter array.
-
-    Returns
-    -------
-    phase_corr : tf.Operation
-        The op to be run to compute phase correlation. When running the op,
-        values for the following placeholders must be fed:
-        `input/a_placeholder:0`, `input/b_placeholder:0`,
-        `input/filter_placeholder:0`.
-    """
-    with tf.name_scope('input'):
-        aph = tf.placeholder(dtype=tf.uint16, shape=ashape,
-                             name='a_placeholder')
-        bph = tf.placeholder(dtype=tf.uint16, shape=bshape,
-                             name='b_placeholder')
-
-        my_filter_t = tf.placeholder(dtype=tf.float32, shape=filter_shape,
-                                     name='filter_placeholder')
-
-        at = tf.to_float(aph)
-        bt = tf.to_float(bph)
-
-    with tf.name_scope('subtract_mean'):
-        at -= tf.reduce_mean(at)
-        bt -= tf.reduce_mean(bt)
-
-    with tf.name_scope('window_filter'):
-        at = at * my_filter_t
-        bt = bt * my_filter_t
-
-    with tf.name_scope('FFT'):
-        ac = tf.cast(at, tf.complex64, name='to_complex')
-        bc = tf.cast(bt, tf.complex64, name='to_complex')
-
-        aft = tf.fft3d(ac)
-        bft = tf.fft3d(bc)
-
-    with tf.name_scope('cross_power_spectrum'):
-        prod = aft * tf.conj(bft)
-        prodnorm = tf.abs(prod, name='norm')
-        ratio = prod / tf.cast(prodnorm, tf.complex64, name='to_complex')
-
-    with tf.name_scope('phase_correlation'):
-        phase_corr = tf.real(tf.ifft3d(ratio))
-
-    return phase_corr
 
 
 def find_phase_corr_max(phase_corr, max_shift_x=100, max_shift_y=100):
@@ -149,44 +93,10 @@ def find_phase_corr_max(phase_corr, max_shift_x=100, max_shift_y=100):
     return dz, dy, dx
 
 
-def xcorr2d_op(ashape, bshape):
-    """Construct a TensorFlow op to compute 2D cross-correlation.
 
-    Parameters
-    ----------
-    ashape : tuple of ints
-        Shape of input array.
-    bshape : tuple of ints
-        Shape of input array.
 
-    Returns
-    -------
-    tf.Operation
-        The op to be run to compute 2D cross-correlation. When running the op,
-        values for the following placeholders must be fed:
-        `input/a_placeholder:0`, `input/b_placeholder:0`.
-    """
-    with tf.name_scope('input'):
-        at = tf.placeholder(dtype=tf.float32, shape=ashape,
-                            name='a_placeholder')
-        bt = tf.placeholder(dtype=tf.float32, shape=bshape,
-                            name='b_placeholder')
 
-        at = tf.expand_dims(at, 0)  # add dummy batch dimension
-        at = tf.expand_dims(at, -1)  # add dummy in_channels dimension
 
-        bt = tf.expand_dims(bt, -1)  # add dummy in_channels dimension
-        bt = tf.expand_dims(bt, -1)  # add dummy out_channels dimension
-
-    with tf.name_scope('subtract_mean'):
-        at -= tf.reduce_mean(at)
-        bt -= tf.reduce_mean(bt)
-
-    conv = tf.nn.conv2d(at, bt, padding='VALID',
-                        strides=[1, 1, 1, 1])
-    conv = tf.squeeze(conv)
-
-    return conv
 
 
 def overlap_score(alayer, blayer, dz, dy, dx):
@@ -241,7 +151,7 @@ def overlap_score(alayer, blayer, dz, dy, dx):
     aframe_roi = aframe_roi[-dy:, ax_min:ax_max].astype(np.float32)
     bframe_roi = bframe_roi[:dy, bx_min:bx_max].astype(np.float32)
 
-    score = xcorr2d_op(aframe_roi.shape, bframe_roi.shape)
+    score = ops.xcorr2d_op(aframe_roi.shape, bframe_roi.shape)
 
     with tf.Session(config=sess_config) as sess:
         score = sess.run(score, feed_dict={
@@ -297,7 +207,7 @@ def stitch(aname, bname, z_min, z_max, overlap, axis=1, max_shift_x=100,
 
     my_filter = hamming_window(*alayer.shape).astype(np.float32)
 
-    phase_corr = phase_corr_op(alayer.shape, blayer.shape, my_filter.shape)
+    phase_corr = ops.phase_corr_op(alayer.shape, blayer.shape, my_filter.shape)
 
     with tf.Session(config=sess_config) as sess:
         phase_corr = sess.run(phase_corr, feed_dict={
