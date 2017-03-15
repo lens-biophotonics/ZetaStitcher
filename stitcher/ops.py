af75e5881a621b61fa70aa0d8edf53fde3ab5d9f
Giacomo Mazzamuto <mazzamuto@lens.unifi.it>
2017-03-15 17:56:30 +0100

    add normxcorr2 op

--- stitcher/ops.py
+++ stitcher/ops.py
@@ -104,3 +104,55 @@ def phase_corr_op(ashape, bshape, filter_shape=None):
         phase_corr = tf.sqrt(phase_corr)
 
     return phase_corr
+
+
+def normxcorr2(ashape, bshape):
+    """Construct a Tensorflow op to compute normalized cross correlation (2D).
+
+    At the moment this op does not perform as fast as expected.
+
+    Parameters
+    ----------
+    ashape : tuple
+        must be len(ashape) = 3
+    bshape :
+        must be len(bshape) = 3 and bshape[0] = 1
+
+    Returns
+    -------
+    tf.Operation
+        The op to be run to compute normalized 2D cross-correlation. When
+        running the op, values for the following placeholders must be fed:
+        `input/a_ph:0`, `input/b_ph:0`.
+    """
+    with tf.name_scope('input'):
+        at = tf.placeholder(dtype=tf.float32, shape=ashape, name='a_ph')
+        bt = tf.placeholder(dtype=tf.float32, shape=bshape, name='b_ph')
+
+    A = tf.constant(bshape[1] * bshape[2], dtype=tf.float32)  # area
+    sums_b = tf.reduce_sum(bt)  # scalar
+    sums_b2 = tf.reduce_sum(tf.square(bt))  # scalar
+
+    with tf.name_scope('expand_dims'):
+        at = tf.expand_dims(at, 0)  # add dummy batch dimension
+        at = tf.expand_dims(at, -1)  # add dummy in_channels dimension
+
+        bt = tf.expand_dims(bt, -1)  # add dummy in_channels dimension
+        bt = tf.expand_dims(bt, -1)  # add dummy out_channels dimension
+
+    b1 = tf.ones(bshape + (1, 1))
+
+    # conv3d is faster than batches of conv2d
+    sums_a = tf.nn.conv3d(at, b1, strides=[1, 1, 1, 1, 1], padding='VALID',
+                          name='sums_a')
+    sums_a2 = tf.nn.conv3d(tf.square(at), b1, strides=[1, 1, 1, 1, 1],
+                           padding='VALID', name='sums_a2')
+    conv = tf.nn.conv3d(at, bt, strides=[1, 1, 1, 1, 1], padding='VALID',
+                        name='myconv')
+
+    num = conv - sums_b * sums_a / A
+    denom = tf.sqrt(
+        (sums_a2 - tf.square(sums_a) / A) * (sums_b2 - tf.square(sums_b) / A))
+
+    normxcorr = tf.squeeze(num / denom)
+    return normxcorr
