be3476b9cc29aeee9eeb20d471911f5161514e1a
Giacomo Mazzamuto <mazzamuto@lens.unifi.it>
2017-03-29 17:37:10 +0200

    runner: WIP: use queues to prepare data

--- stitcher/runner.py
+++ stitcher/runner.py
@@ -1,17 +1,20 @@
 import sys
+import queue
 import os.path
 import argparse
+import threading
 
 import numpy as np
 
 import colored
 from colored import stylize
 
-from .core import stitch
+from .core import normxcorr2_fftw
 from .filematrix import FileMatrix
+from .inputfile import InputFile
 
 
-def main():
+def parse_args():
     parser = argparse.ArgumentParser(
         description='''
 Stitch tiles in a folder.
@@ -23,7 +26,7 @@ The following naming conventions are used:
 * X is the direction orthogonal to Y in the frame plane (X, Y).
 
 Unless otherwise stated, all values are expected in px.
-''',
+    ''',
         epilog='Author: Giacomo Mazzamuto <mazzamuto@lens.unifi.it>',
         formatter_class=argparse.RawTextHelpFormatter)
 
@@ -63,8 +66,13 @@ Unless otherwise stated, all values are expected in px.
     group.add_argument('--stride', type=int, default=200,
                        help='stride used for multiple Z sampling')
 
-    arg = parser.parse_args(sys.argv[1:])
+    parser.add_argument('-n', type=int, default=1,
+                        help='number of parallel threads to use')
 
+    return parser.parse_args(sys.argv[1:])
+
+
+def build_queue(arg):
     fm = FileMatrix(arg.input_folder)
     fm.ascending_tiles_X = True
     fm.ascending_tiles_Y = False
@@ -73,8 +81,10 @@ Unless otherwise stated, all values are expected in px.
     stitch_axis = [2, 1]
     overlap = [arg.overlap_h, arg.overlap_v]
 
+    q = queue.Queue()
+
     for group_generator, axis, overlap in zip(
-            group_generators, stitch_axis, overlap):
+                group_generators, stitch_axis, overlap):
         for group in group_generator:
 
             tile_generator = group.itertuples()
@@ -82,36 +92,127 @@ Unless otherwise stated, all values are expected in px.
             atile = next(tile_generator)
 
             for btile in tile_generator:
-                temp = map(os.path.basename,
-                           (atile.filename, btile.filename))
-                temp = list(temp)
-                print(
-                    stylize('stitching {} {}, axis = {}'.format(*temp, axis),
-                            colored.fg('magenta')))
                 central_frame = atile.nfrms // 2
                 start_frame = (central_frame
                                - (arg.z_samples // 2 * arg.stride)
                                + (0 if arg.z_samples % 2 else arg.stride // 2))
-                results = []
                 for i in range(0, arg.z_samples):
                     z_frame = start_frame + i * arg.stride
-                    ret = stitch(
-                        atile.filename, btile.filename, z_frame, axis=axis,
-                        overlap=overlap, max_shift_z=arg.max_dz,
-                        max_shift_y=arg.max_dy, max_shift_x=arg.max_dx)
-                    results.append(ret)
-                print('results: ' + str(results))
-                results = np.array(results)
-                if arg.average:
-                    results = np.average(
-                        results, axis=0, weights=results[:, 3])
-                else:
-                    results = results[np.argmax(results[:, 3]), :]
-                print('results: ' + str(results))
+                    params_dict = {
+                        'aname': atile.filename,
+                        'bname': btile.filename,
+                        'z_frame': z_frame,
+                        'axis': axis,
+                        'overlap': overlap,
+                        'max_shift_z': arg.max_dz,
+                        'max_shift_y': arg.max_dy,
+                        'max_shift_x': arg.max_dx
+                    }
+                    q.put(params_dict)
                 atile = btile
+    return q
 
-            print('===================================')
 
+def main():
+    def worker():
+        while True:
+            item = data_queue.get()
+            if item is None:
+                break
+            try:
+                aname = item[0]
+                bname = item[1]
+                axis = item[2]
+                alayer = item[3]
+                blayer = item[4]
+
+                xcorr = normxcorr2_fftw(alayer, blayer)
+
+                shift = list(np.unravel_index(np.argmax(xcorr), xcorr.shape))
+                score = xcorr[tuple(shift)]
+
+                print(100 * (1 - q.qsize() / initial_queue_length))
+                output_q.put([aname, bname, axis] + shift + [score])
+            finally:
+                data_queue.task_done()
+                q.task_done()
+
+    arg = parse_args()
+
+    q = build_queue(arg)
+    initial_queue_length = q.qsize()
+    data_queue = queue.Queue(maxsize=int(arg.n * 2))
+    output_q = queue.Queue()
+    threads = []
+    for i in range(arg.n):
+        t = threading.Thread(target=worker)
+        t.start()
+        threads.append(t)
+
+    while True:
+        try:
+            item = q.get_nowait()
+        except queue.Empty:
+            break
+        aname = item['aname']
+        bname = item['bname']
+        max_shift_z = item['max_shift_z']
+        max_shift_y = item['max_shift_y']
+        max_shift_x = item['max_shift_x']
+        z_frame = item['z_frame']
+        axis = item['axis']
+        overlap = item['overlap']
+
+        a = InputFile(aname)
+        b = InputFile(bname)
+
+        a.channel = 1
+        b.channel = 1
+
+        z_min = z_frame - max_shift_z
+        z_max = z_frame + max_shift_z + 1
+
+        alayer = a.layer(z_min, z_max, dtype=np.float32)
+        if axis == 2:
+            alayer = np.rot90(alayer, axes=(1, 2))
+        alayer = alayer[:, -overlap:, :]
+
+        blayer = b.layer_idx(z_frame, dtype=np.float32)
+        if axis == 2:
+            blayer = np.rot90(blayer, axes=(1, 2))
+        blayer = blayer[:, 0:overlap, :]
+
+        half_max_shift_x = max_shift_x // 2
+
+        blayer = blayer[:, :-max_shift_y, half_max_shift_x:-half_max_shift_x]
+
+        data_queue.put([aname, bname, axis, alayer, blayer])
+
+    # block until all tasks are done
+    data_queue.join()
+
+    # stop workers
+    for i in range(arg.n):
+        data_queue.put(None)
+    for t in threads:
+        t.join()
+
+    for el in list(output_q.queue):
+        print(el)
 
 if __name__ == '__main__':
     main()
+
+    # print(
+    #     stylize('stitching {} {}, axis = {}'.format(*temp, axis),
+    #             colored.fg('magenta')))
+
+    # results.append(ret)
+    # print('results: ' + str(results))
+    # results = np.array(results)
+    # if arg.average:
+    #     results = np.average(
+    #         results, axis=0, weights=results[:, 3])
+    # else:
+    #     results = results[np.argmax(results[:, 3]), :]
+    # print('results: ' + str(results))
