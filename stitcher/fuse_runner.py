09ffc1ccdfcc997f38e0dcf507be4ff175ea20d2
Giacomo Mazzamuto <mazzamuto@lens.unifi.it>
2017-04-08 19:12:46 +0200

    Minimum spanning tree: do not pass label

--- stitcher/fuse_runner.py
+++ stitcher/fuse_runner.py
@@ -52,14 +52,20 @@ class FuseRunner(object):
         df = self.df
         T = self.minimum_spanning_tree
 
+        df_ab = df.set_index(['aname', 'bname'])
+
         fm_df['Xs'] = 0
         fm_df['Ys'] = 0
         fm_df['Zs'] = 0
         fm_df['weight'] = 0
         for edge in nx.dfs_edges(T, source=fm_df.iloc[0].name):
-            edge_data = T.get_edge_data(*edge)
-            if df.loc[edge_data['label']]['axis'] == 2:
-                stride_y = fm_df.ix[edge[1], 'xsize']
+            try:
+                row = df_ab.loc[edge]
+            except KeyError:
+                row = df_ab.loc[edge[::-1]]
+            axis = row['axis']
+            if axis == 2:
+                stride_y = fm_df.ix[edge[0], 'xsize']
                 key_Y = 'X'
 
                 key_Xs = 'Ys'
@@ -67,7 +73,7 @@ class FuseRunner(object):
 
                 ascending_sign = 1 if self.fm.ascending_tiles_X else -1
             else:
-                stride_y = fm_df.ix[edge[1], 'ysize']
+                stride_y = fm_df.ix[edge[0], 'ysize']
                 key_Y = 'Y'
 
                 key_Xs = 'Xs'
@@ -76,21 +82,19 @@ class FuseRunner(object):
                 ascending_sign = 1 if self.fm.ascending_tiles_Y else -1
 
             sign_y = (1 if fm_df.ix[edge[1], key_Y] >= fm_df.ix[edge[0], key_Y]
-                      else -1) * ascending_sign
+                    else -1) * ascending_sign
             sign_z = (1 if fm_df.ix[edge[1], 'Z'] >= fm_df.ix[edge[0], 'Z']
-                      else -1)
+                    else -1)
 
             # absolute stitched positions
-            Xs = (fm_df.ix[edge[0], key_Xs] + df.loc[edge_data['label']]['dx'])
-            Ys = (fm_df.ix[edge[0], key_Ys]
-                  + sign_y * (stride_y - df.loc[edge_data['label']]['dy']))
-            Zs = (fm_df.ix[edge[0], 'Zs']
-                  + sign_z * df.loc[edge_data['label']]['dz'])
+            Xs = (fm_df.ix[edge[0], key_Xs] + row['dx'])
+            Ys = (fm_df.ix[edge[0], key_Ys] + sign_y * (stride_y - row['dy']))
+            Zs = (fm_df.ix[edge[0], 'Zs'] + sign_z * row['dz'])
 
             fm_df.ix[edge[1], key_Xs] = Xs
             fm_df.ix[edge[1], key_Ys] = Ys
             fm_df.ix[edge[1], 'Zs'] = Zs
-            fm_df.ix[edge[1], 'weight'] = edge_data['weight']
+            fm_df.ix[edge[1], 'weight'] = 1 - row['score']
 
         for key in ['Xs', 'Ys', 'Zs']:
             fm_df[key] -= fm_df[key].min()
@@ -99,13 +103,10 @@ class FuseRunner(object):
     def minimum_spanning_tree(self):
         df = self.df
         G = nx.Graph()
-        for index, row in df.iterrows():
-            G.add_node(row['aname'])
-            G.add_node(row['bname'])
 
         G.add_edges_from(((
-            u, v, {'weight': d, 'label': i}) for u, v, d, i in
-            np.c_[df['aname'], df['bname'], 1 - df['score'], df.index]))
+            u, v, {'weight': w}) for u, v, w in
+            np.c_[df['aname'], df['bname'], 1 - df['score']]))
 
         T = nx.minimum_spanning_tree(G)
         return T
