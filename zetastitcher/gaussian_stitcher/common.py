1832268602d75e89af324d36bc8aca4a88b19e31
Giacomo Mazzamuto <mazzamuto@lens.unifi.it>
2018-07-26 12:49:25 +0200

    gaussian stitcher: add regularization to Lambda

--- zetastitcher/gaussian_stitcher/common.py
+++ zetastitcher/gaussian_stitcher/common.py
@@ -1,5 +1,7 @@
+import numpy as np
 
-def ConstraintTuple(ndims):
+
+def ConstraintTuple(ndims, eps_reg=None):
     class ConstraintTupleND(object):
         __slots__ = ['v', 'w', 'Lambda', 'p', 'ub', 'lb']
         def __init__(self, v, w, Lambda, p, ub=None, lb=None):
@@ -10,6 +12,10 @@ def ConstraintTuple(ndims):
             self.ub = ub
             self.lb = lb
             self._check_dims(Lambda, p, ub, lb)
+            if eps_reg is not None:
+                if eps_reg < 0:
+                    raise ValueError('eps_reg < 0')
+                self.Lambda += eps_reg * np.eye(Lambda.shape[0])
 
         def _check_dims(self, Lambda, p, ub, lb):
             if Lambda.shape != (ndims, ndims):
