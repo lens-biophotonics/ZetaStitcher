789a87328ef1f096ea96e287b71020f922c3b253
Giacomo Mazzamuto <giacomo.mazzamuto@cnr.it>
2020-07-01 10:58:50 +0200

    ZipWrapper: use lru_cache and ProcessPoolExecutor
    TODO: pass cache size as parameter, enable / diable cache

--- zetastitcher/io/zipwrapper.py
+++ zetastitcher/io/zipwrapper.py
@@ -2,9 +2,9 @@ import gc
 import sys
 import ctypes
 import zipfile
-import threading
-import multiprocessing as mp
-from os import cpu_count
+import functools
+import concurrent.futures
+import multiprocessing.heap as mph
 from pathlib import Path
 
 import imageio
@@ -20,6 +20,22 @@ def get_typecodes():
     ]
 
     return {np.dtype(ctype).str: ctype for ctype in simple_types}
+
+
+e = concurrent.futures.ProcessPoolExecutor()
+
+
+@functools.lru_cache(2000)
+def work(fname, internal_fname, dtype=None):
+    return e.submit(imread_wrapper, fname, internal_fname, dtype)
+
+
+def imread_wrapper(fname, internal_fname, dtype=None):
+    zf = zipfile.ZipFile(str(fname), mode='r')
+    a = imageio.imread(zf.read(internal_fname))
+    if dtype is not None:
+        a = a.astype(dtype)
+    return a
 
 
 class ZipWrapper(object):
@@ -52,7 +68,7 @@ class ZipWrapper(object):
         self.zf = zipfile.ZipFile(str(self.file_path), mode='r')
         names = self.zf.namelist()
 
-        im = imageio.imread(self.zf.read(names[0]))
+        im = imread_wrapper(self.file_path, names[0])
 
         self.xsize = im.shape[-1]
         self.ysize = im.shape[-2]
@@ -71,90 +87,31 @@ class ZipWrapper(object):
         return a
 
     def zslice(self, start_frame, end_frame=None, dtype=None, copy=None):
-        def worker(outarr):
-            while True:
-                got = input_q.get()
-                if got is None:
-                    return
-
-                data, c, i = got
-
-                a = imageio.imread(data)
-                if dtype is not None:
-                    a = a.astype(dtype)
-
-                outarr[i] = a
-                output_q.put((c, i))
-
-        def process_output_queue():
-            while True:
-                got = output_q.get()
-                if got is None:
-                    break
-                c, i = got
-                out[c] = a[i]
-                available_q.put(i)
-
-        def populate_input_queue():
-            for c in range(s[0]):
-                i = available_q.get()
-                data = self.zf.read(self.file_name_fmt.format(start_frame + c))
-                input_q.put((data, c, i))
-            for _ in range(n_of_procs):
-                input_q.put(None)
-
         if dtype is None:
             dtype = self.dtype
 
-        maxsize = cpu_count()
-        available_q = mp.Queue(maxsize=maxsize)
-        input_q = mp.Queue(maxsize=maxsize)
-        output_q = mp.Queue()
-
-        poq = threading.Thread(target=process_output_queue)
-        poq.start()
-
-        procs = []
         s = list(self.shape)
         s[0] = end_frame - start_frame
 
         out = np.zeros(s, dtype)
 
-        temp_s = list(self.shape)
-        temp_s[0] = maxsize
+        # NEW ##################################
 
-        tc = get_typecodes()
-        typestr = tc[np.dtype(dtype).str]
+        my_futures = []
 
-        m_array = mp.Array(typestr, int(np.prod(temp_s)))
-        a = np.frombuffer(m_array.get_obj(), dtype=typestr).reshape(temp_s)
+        for c in range(s[0]):
+            internal_fname = self.file_name_fmt.format(start_frame + c)
+            fut = work(self.file_path, internal_fname, dtype)
+            my_futures.append(fut)
 
-        n_of_procs = min(maxsize, s[0])
+        for c, fut in zip(range(s[0]), my_futures):
+            out[c] = fut.result(None)
 
-        for p in range(n_of_procs):
-            wp = mp.Process(target=worker, args=(a,))
-            wp.start()
-            procs.append(wp)
-
-            available_q.put(p)
-
-        piq = threading.Thread(target=populate_input_queue)
-        piq.start()
-        piq.join()
-
-        for p in procs:
-            p.join()
-            del p
-
-        output_q.put(None)
-        poq.join()
-
-        del a
-        del m_array
+        print(work.cache_info())
 
         # force release of shared memory for Python < 3.8
         if sys.version_info < (3, 8):
-            mp.heap.BufferWrapper._heap = mp.heap.Heap()
+            mph.BufferWrapper._heap = mph.Heap()
             gc.collect()
 
         return out
